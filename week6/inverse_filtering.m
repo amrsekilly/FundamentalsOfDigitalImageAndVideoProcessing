2aa24ea2dbf77d7fe4ed640bcd6d0eace1981d6c
Amr <amr.e.dev@gmail.com>
2015-05-17 06:56:07 +0300

    Best visual result at T=0.5

--- week6/inverse_filtering.m
+++ week6/inverse_filtering.m
@@ -5,7 +5,7 @@ close all
 clc
 
 % specify the threshold T
-T = 7e-1;
+T = 5e-1;
 
 %% read in the original, sharp and noise-free image
 original = im2double(rgb2gray((imread('original_cameraman.jpg'))));
